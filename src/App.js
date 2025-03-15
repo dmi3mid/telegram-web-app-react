019cb610e1d2f22ad208e6c964d540f7be7d65eb
dmi3mid <dmi3midd@gmail.com>
2025-03-15 18:28:57 +0200

    Add close-button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 const tg = window.Telegram.WebApp;
 function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close()
+  }
   return (
     <div className="App">
       Work
+      <button onClick={onClose}>Close</button>
     </div>
   );
 }
