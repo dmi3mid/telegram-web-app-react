c8c28c09b01a7124cec7f71b967b56fe706f34a8
dmi3mid <dmi3midd@gmail.com>
2025-03-17 00:14:39 +0200

    Fix

--- src/componets/ProductList/ProductList.jsx
+++ src/componets/ProductList/ProductList.jsx
@@ -7,13 +7,9 @@ import classes from './ProductList.module.css';
 export default function ProductList({products}) {
   return (
     <div className={classes.productList}>
-        {products.map((product, index) => {
-          <ProductItem 
-            key={product.id}
-            number={index+1}
-            product={product}
-          />
-        })}
+        {products.map((product, index) => (
+          <ProductItem key={product.id} number={index+1} product={product}/>
+        ))}
     </div>
   )
 }
