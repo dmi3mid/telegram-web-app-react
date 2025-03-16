2955358da3eaee30a13404b2107796b2805d24e8
dmi3mid <dmi3midd@gmail.com>
2025-03-16 15:05:49 +0200

    Remove font style

--- src/componets/ProductList/ProductList.jsx
+++ src/componets/ProductList/ProductList.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import classes from './ProductList.module.css';
-import '../../styles/TgFontStyle.css';
 
 export default function ProductList() {
   return (
-    <div className={[classes.ProductList, 'roboto-font'].join(' ')}>
+    <div className={classes.ProductList}>
         ProductList
     </div>
   )
--- src/componets/input/TgInput.jsx
+++ src/componets/input/TgInput.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
 import classes from './TgInput.module.css';
-import '../../styles/TgFontStyle.css';
 
 export default function TgInput(props) {
   return (
-    <input {...props} className={[classes.TgInput, 'roboto-font'].join(' ')}/>
+    <input {...props} className={classes.TgInput}/>
   )
 }
