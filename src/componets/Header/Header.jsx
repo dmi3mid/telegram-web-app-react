56efeadbdf1b794d2f65d55715498340fb311b25
dmi3mid <dmi3midd@gmail.com>
2025-03-17 22:45:31 +0200

    Add styles

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-// import useTelegram from '../../hooks/useTelegram';
+import useTelegram from '../../hooks/useTelegram';
 
 import classes from './Header.module.css';
 
 export default function Header() {
-    // const {user} = useTelegram();
-    const tg = window.Telegram.WebApp;
+    const {user} = useTelegram();
     return (
         <div className={classes.header}>
             <h1 className={classes.headerTitle}>
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </h1>
-            <p><span class="material-symbols-outlined">shopping_cart_checkout</span></p>
+            <p className={classes.headerShoppinfCart}><span class="material-symbols-outlined">shopping_cart_checkout</span></p>
         </div>
     )
 }
