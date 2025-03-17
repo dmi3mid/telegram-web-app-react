f37f7cc775f371e2933d70e3443a07b772a79068
dmi3mid <dmi3midd@gmail.com>
2025-03-17 21:37:25 +0200

    Remove useStyles

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import useTelegram from '../../hooks/useTelegram';
-import useStyles from '../../hooks/useStyles';
 
 export default function Header() {
-    const {shoppingCartCheckout, materialSymbolsOutlined} = useStyles();
     const {user} = useTelegram();
     return (
         <div>
             <h1>{user.username ? user.username : `User${user.id}`}</h1>
-            <p><span class={materialSymbolsOutlined}>{shoppingCartCheckout}</span></p>
+            <p><span class="material-symbols-outlined">shopping_cart_checkout</span></p>
         </div>
     )
 }
