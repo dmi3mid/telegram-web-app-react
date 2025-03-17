4bdb36486814e5b6632efe8030a67b1a54c24c48
dmi3mid <dmi3midd@gmail.com>
2025-03-17 22:29:21 +0200

    Fix

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-// import useTelegram from '../../hooks/useTelegram';
+import useTelegram from '../../hooks/useTelegram';
 
 import classes from './Header.module.css';
 
 export default function Header() {
-    // const {user} = useTelegram();
+    const {user} = useTelegram();
     return (
         <div className={classes.header}>
             <h1 className={classes.headerTitle}>
-                User
-                {/* {user.username ? user.username : `User${user.id}`}&nbsp; */}
-                <span class="material-symbols-outlined">shopping_cart_checkout</span>
+                {user?.username}
             </h1>
+            <p><span class="material-symbols-outlined">shopping_cart_checkout</span></p>
         </div>
     )
 }
