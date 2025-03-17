b74b288185e8622e98bce0aa640860f65af536ef
dmi3mid <dmi3midd@gmail.com>
2025-03-17 22:05:39 +0200

    Add Header component

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import useTelegram from '../../hooks/useTelegram';
 
+import classes from './Header.module.css';
+
 export default function Header() {
     const {user} = useTelegram();
     return (
-        <div>
-            <h1>{user.username ? user.username : `User${user.id}`}</h1>
-            <p><span class="material-symbols-outlined">shopping_cart_checkout</span></p>
+        <div className={classes.header}>
+            <h1 className={classes.headerTitle}>{user.username ? user.username : `User${user.id}`}&nbsp;<span class="material-symbols-outlined">shopping_cart_checkout</span></h1>
+            {/* <p><span class="material-symbols-outlined">shopping_cart_checkout</span></p> */}
         </div>
     )
 }
--- src/componets/ProductList/ProductList.jsx
+++ src/componets/ProductList/ProductList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 import ProductItem from '../ProductItem/ProductItem';
+import Header from '../Header/Header';
 
 import classes from './ProductList.module.css';
 
 export default function ProductList({products}) {
   return (
-    <div className={classes.productList}>
+    <div className="">
+      <Header/>
+      <div className={classes.productList}>
         {products.map((product, index) => (
           <ProductItem key={product.id} number={index+1} product={product}/>
         ))}
+      </div>
     </div>
+
   )
 }
