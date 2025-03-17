dcd52955cb96fa9e22b5cda21d49e6ee5e65fc43
dmi3mid <dmi3midd@gmail.com>
2025-03-17 22:35:04 +0200

    Fix

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import useTelegram from '../../hooks/useTelegram';
+// import useTelegram from '../../hooks/useTelegram';
 
 import classes from './Header.module.css';
 
 export default function Header() {
-    const {user} = useTelegram();
-    console.log(user);
+    // const {user} = useTelegram();
+    const tg = window.Telegram.WebApp;
     return (
         <div className={classes.header}>
             <h1 className={classes.headerTitle}>
-                {user?.username}
+                {tg.initDataUnsafe?.user?.username}
             </h1>
             <p><span class="material-symbols-outlined">shopping_cart_checkout</span></p>
         </div>
