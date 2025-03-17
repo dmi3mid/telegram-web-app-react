4c03258437edb558cee3162143650c49dc7fa189
dmi3mid <dmi3midd@gmail.com>
2025-03-17 23:15:18 +0200

    Add remove button to ProductItem

--- src/componets/ProductItem/ProductItem.jsx
+++ src/componets/ProductItem/ProductItem.jsx
@@ -11,6 +11,7 @@ export default function ProductItem({number, product}) {
       <img src="" alt="" />
       <div className={classes.productDescription}>{product.description}</div>
       <TgButton>Add {product.price}$</TgButton>
+      <TgButton>Remove</TgButton>
     </div>
   )
 }
